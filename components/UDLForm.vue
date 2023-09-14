980db91fe9a5952c4e0b436781f4874a3018cf9c
Nopfed <robby.tsunami@gmail.com>
2023-09-14 19:52:35 -0400

    WIP: Needs sub-fields and separation logic for license sub-tags

--- components/UDLForm.vue
+++ components/UDLForm.vue
@@ -26,6 +26,69 @@
     class="switch"
     label="Derivation with License Passthrough"
   />
+  <v-switch 
+    v-if="modelValue.useUdl"
+    v-model="modelValue.commercialUse"
+    color="red-darken-4"
+    class="switch"
+    label="Commercial Use Allowed"
+  />
+  <v-switch
+    v-if="modelValue.useUdl"
+    v-model="modelValue.commercialUseWithCredit"
+    color="red-darken-4"
+    class="switch"
+    label="Commercial Use Allowed With Credit"
+  />
+  <v-switch
+    v-if="modelValue.useUdl"
+    v-model="modelValue.monthlyLicenseFee"
+    color="red-darken-4"
+    class="switch"
+    label="Monthly License Fee"
+  />
+  <v-switch
+    v-if="modelValue.useUdl"
+    v-model="modelValue.oneTimeLicenseFee"
+    color="red-darken-4"
+    class="switch"
+    label="One Time License Fee"
+  />
+  <v-switch
+    v-if="modelValue.useUdl"
+    v-model="modelValue.currency"
+    color="red-darken-4"
+    class="switch"
+    label="Currency"
+  />
+  <v-switch
+    v-if="modelValue.useUdl"
+    v-model="modelValue.licenseDuration"
+    color="red-darken-4"
+    class="switch"
+    label="License Duration"
+  />
+  <v-switch
+    v-if="modelValue.useUdl"
+    v-model="modelValue.paymentAddress"
+    color="red-darken-4"
+    class="switch"
+    label="Payment Address"
+  />
+  <v-switch
+    v-if="modelValue.useUdl"
+    v-model="modelValue.paymentModeRandom"
+    color="red-darken-4"
+    class="switch"
+    label="Payment Mode Random Distribution"
+  />
+  <v-switch
+    v-if="modelValue.useUdl"
+    v-model="modelValue.paymentModeGlobal"
+    color="red-darken-4"
+    class="switch"
+    label="Payment Mode Global Distribution"
+  />
   <a
     target="_blank" 
     href="https://arwiki.wiki/#/en/Universal-Data-License-How-to-use-it"
@@ -44,9 +107,10 @@ watch(props.modelValue, () => {
   console.log(props.modelValue)
 
   if (!props.modelValue.useUdl) {
-    props.modelValue.useDerivationCredit = false
-    props.modelValue.useDerivationIndication = false
-    props.modelValue.useDerivationPassthrough = false
+    let x: any
+    for (x in props.modelValue) {
+      x = false
+    }
   }
 })
 </script>
--- utils/udl.ts
+++ utils/udl.ts
@@ -3,4 +3,13 @@ export type UDLOptions = {
   useDerivationCredit?: boolean
   useDerivationIndication?: boolean
   useDerivationPassthrough?: boolean
+  commercialUse?: boolean
+  commercialUseWithCredit?: boolean
+  monthlyLicenseFee?: boolean
+  oneTimeLicenseFee?: boolean
+  currency?: boolean
+  licenseDuration?: boolean
+  paymentAddress?: boolean
+  paymentModeRandom?: boolean
+  paymentModeGlobal?: boolean
 }
